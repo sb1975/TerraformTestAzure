b3a93d7ff3cafb5d36b9cce6c487a0a581301dee
sudeepbatra <16325484+sb1975@users.noreply.github.com>
2021-08-14 00:05:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "jonnychipz-infra"
-    storage_account_name = "jonnychipztstate"
+    resource_group_name  = "rg01"
+    storage_account_name = "saterraformstate0813"
     container_name       = "tstate"
-    key                  = "77Q4LUB5o9wRdbPYDt+0kGZP+L8Sj9E/FNXg7lZBQS5z3mLod5cyan4wA19CR1SmlqIRUFQfhuQrPVaGzNhjGw=="
+    key                  = "t2jx5f4cJUGXFw8A8Y8DL3kYdRfGVXKK9Xp/TKR7wtllBGLDYhbMFBQ8XeiG7DbCEICtLfHFmxQcnJzuobjfBA=="
   }
 
   required_providers {
     azurerm = {
       # Specify what version of the provider we are going to utilise
       source  = "hashicorp/azurerm"
-      version = ">= 2.4.1"
+      version = ">= 2.71.0"
     }
   }
 }
@@ -22,39 +22,39 @@ provider "azurerm" {
   }
 }
 data "azurerm_client_config" "current" {}
-# Create our Resource Group - Jonnychipz-RG
+# Create our Resource Group - rg01-app01
 resource "azurerm_resource_group" "rg" {
-  name     = "jonnychipz-app01"
-  location = "UK South"
+  name     = "rg01-app01"
+  location = "East US"
 }
-# Create our Virtual Network - Jonnychipz-VNET
+# Create our Virtual Network - terraformvnet
 resource "azurerm_virtual_network" "vnet" {
-  name                = "jonnychipzvnet"
+  name                = "terraformvnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 # Create our Subnet to hold our VM - Virtual Machines
 resource "azurerm_subnet" "sn" {
-  name                 = "VM"
+  name                 = "SubnetVM01"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-# Create our Azure Storage Account - jonnychipzsa
-resource "azurerm_storage_account" "jonnychipzsa" {
-  name                     = "jonnychipzsa"
+# Create our Azure Storage Account - terraformsa
+resource "azurerm_storage_account" "sa" {
+  name                     = "terraformsa"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   tags = {
-    environment = "jonnychipzenv1"
+    environment = "terraformenv1"
   }
 }
 # Create our vNIC for our VM and assign it to our Virtual Machines Subnet
 resource "azurerm_network_interface" "vmnic" {
-  name                = "jonnychipzvm01nic"
+  name                = "terraformvm01nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -64,9 +64,9 @@ resource "azurerm_network_interface" "vmnic" {
     private_ip_address_allocation = "Dynamic"
   }
 }
-# Create our Virtual Machine - Jonnychipz-VM01
-resource "azurerm_virtual_machine" "jonnychipzvm01" {
-  name                  = "jonnychipzvm01"
+# Create our Virtual Machine - Terraform-VM01
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "terraformvm01"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.vmnic.id]
@@ -78,14 +78,14 @@ resource "azurerm_virtual_machine" "jonnychipzvm01" {
     version   = "latest"
   }
   storage_os_disk {
-    name              = "jonnychipzvm01os"
+    name              = "Terraformvm01os"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
-    computer_name  = "jonnychipzvm01"
-    admin_username = "jonnychipz"
+    computer_name  = "Terraformvm01"
+    admin_username = "adminuser"
     admin_password = "Password123$"
   }
   os_profile_windows_config {
